755a4f60c929b81eb142d4463195a138af5eb7d5
Nathan Nguyen <zhectx@gmail.com>
2021-11-21 06:46:34 -0500

    Assignment1_Submission
    - Assets provided, GDD To be Added soon

---
+++ Assets/LoginScreenHandler.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginScreenHandler : MonoBehaviour
+{
+
+    private Animator m_animator;
+
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        m_animator = GetComponent<Animator>();
+        m_animator.SetInteger("AnimState", 1);
+        m_animator.SetBool("Grounded", true);
+    }
+
+
+
+}
+
---
+++ Assets/Scripts/UiBehaviour.cs
@@ -0,0 +1,71 @@
+/*
+Nathan Nguyen
+George Brown College
+Assignment 2 - GAME2014-F2021
+
+101268067
+10/23/2021
+
+Description:
+Handles all UI Button Interactions
+
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UiBehaviour : MonoBehaviour
+{
+    private int nextSceneIndex;
+    private int previousSceneIndex;
+
+
+
+    void Start()
+    {
+
+        nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        previousSceneIndex = SceneManager.GetActiveScene().buildIndex - 1;
+    }
+
+    public void OnPauseButtonPressed(GameObject PauseMenu)
+    {
+        Time.timeScale = 0.0f;
+        PauseMenu.SetActive(true);
+    }
+
+    public void OnResumeButtonPressed(GameObject PauseMenu)
+    {
+        Time.timeScale = 1.0f;
+        PauseMenu.SetActive(false);
+    }
+
+    public void Test()
+    {
+        Debug.Log("Test Successful");
+    }
+
+
+    public void OnExitButtonPressed()
+    {
+        Application.Quit();  
+    }
+
+
+    public void ButtonGeneralPressed(string SceneSelect)
+    {
+        SceneManager.LoadScene(SceneSelect);
+        Debug.Log(SceneManager.GetActiveScene().buildIndex);
+    }
+
+
+    public void CanvasSwitch(GameObject canvas)
+    {
+        canvas.SetActive(!canvas.activeSelf);
+    }
+
+
+
+}
